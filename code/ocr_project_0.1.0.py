7184ea749a5021cbc124f5f4b90650ba8e6afbe6
dongwooky <tomiyadragon@gmail.com>
2020-06-03 19:32:48 +0900

    add tesseract-ocr module, begin version 0.1.0

--- code/ocr_project_0.1.0.py
+++ code/ocr_project_0.1.0.py
@@ -1,3 +1,17 @@
+"""
+tesseract-ocr 설치
+
+1) https://github.com/UB-Mannheim/tesseract/wiki 에서 exe 파일 다운로드
+2) 설치 시 "Additional script data" 항목에서 "Hangul Script", "Hangul vertical script" 항목 체크,
+   "Additional language data" 항목에서 "Korean" 항목 체크.
+3) 설치 후 시스템 환경변수 PATH에 Tesseract 설치 폴더 추가
+   (e.g.) c:\Program Files\Tesseract-OCR
+4) 설치 후 시스템 환경변수에 TESSDATA_PREFIX를 추가하고, 변수 값을 <Tesseract-DIR>\tessdata 로 설정
+5) <Tesseract-DIR>\tessdata\script\ 폴더에 있는 Hangul.traineddata, Hangul_vert.traineddata 파일을
+   <Tesseract-DIR>\tessdata\ 폴더로 복사
+6) 명령 프롬프트 창에서 pip install pytesseract 명령 입력
+"""
+
 import cv2
 import numpy as np
 import pytesseract
